5c63e654c6c775f8ae2783e215da1a99b58b4d97
Wolfffff <scott.w.wolf1@gmail.com>
2022-11-02 14:04:52 -0400

    Finding candidate SNPs

---
+++ code/20221101_finding_candidate_snps.R
@@ -0,0 +1,20 @@
+load('../data/200615_binGlmScan_contTimeParam1234_slopePerTreat.RData')
+
+library(tidyverse)
+
+chrs <- c('2L', '2R', '3L', '3R', '4', 'X')
+
+
+signCut <- 1e-09
+nonSignCut <- 1e-4
+
+binGlmScan_contTime_param1234_200615 %>% filter(`generation:treatment_p` < signCut, CHROM %in% chrs, emTrend.HS_p < nonSignCut) %>% count
+
+
+signCut <- 8e-12
+nonSignCut <- 1e-4
+
+candidate_snps <- binGlmScan_contTime_param1234_200615 %>% filter(`generation:treatment_p` < signCut, CHROM %in% chrs, emTrend.HS_p < nonSignCut)
+candidate_snps %>% count
+
+candidate_snps %>% group_by(CHROM) %>% count
