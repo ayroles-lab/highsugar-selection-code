dd3a5f8a3da941ca9b4af7222850f2ff9aae293d
Diogo Melo <diogro@gmail.com>
2023-06-07 13:45:55 -0400

    reproducing DE figure

--- code/20221101_differential_expression.R
+++ code/20221101_differential_expression.R
@@ -1,8 +1,8 @@
 
 ########## 200904 #########
 #Gene lists for Julien
-load('../data/200618_binGlmScan_slopePerTreat_GRangesFormat.RData')
-load('../data/200608_DEres_GRanges.RData')
+load('data/200618_binGlmScan_slopePerTreat_GRangesFormat.RData')
+load('data/200608_DEres_GRanges.RData')
 chrs <- c('2L', '2R', '3L', '3R', '4', 'X', 'Y')
 binGlm.chrs <- as.character(rep.int(binGlmScan_contTime_param1234_200615.gr@seqnames@values, binGlmScan_contTime_param1234_200615.gr@seqnames@lengths))
 binGlmScan_contTime_param1234_200615.gr <- binGlmScan_contTime_param1234_200615.gr[binGlm.chrs %in% chrs]
@@ -16,13 +16,13 @@ DE.body <- dmel.gff_swapGenes_body[dmel.gff_swapGenes_body$padj_geno < .05 | dme
 overlap.body <- findOverlaps(selSNPs, DE.body)
 DE.body_overlap <- DE.body[unique(overlap.body@to)]
 dat <- as.data.frame(DE.body_overlap[,c(1:5, 61:74)])
-write.table(dat, file = '../figures/genes_DEgenoORdietFDR05_selP8e12_body', quote = F, sep = '\t', row.names = F)
+#write.table(dat, file = '../figures/genes_DEgenoORdietFDR05_selP8e12_body', quote = F, sep = '\t', row.names = F)
 
 DE.head <- dmel.gff_swapGenes_head[dmel.gff_swapGenes_head$padj_geno < .05 | dmel.gff_swapGenes_head$padj_diet < .05]
 overlap.head <- findOverlaps(selSNPs, DE.head)
 DE.head_overlap <- DE.head[unique(overlap.head@to)]
 dat <- as.data.frame(DE.head_overlap[,c(1:5, 61:74)])
-write.table(dat, file = '../figures/genes_DEgenoORdietFDR05_selP8e12_head', quote = F, sep = '\t', row.names = F)
+#write.table(dat, file = '../figures/genes_DEgenoORdietFDR05_selP8e12_head', quote = F, sep = '\t', row.names = F)
 
 #Figure 4 (or 5 depending on the paper structure)
 #Count DE genes & overlaps
@@ -38,94 +38,89 @@ length(head.DEgeno)
 length(head.DEdiet)
 sum(head.DEgeno %in% head.DEdiet)
 
-load('../data/200904_swapExp_marginalDE_varyingPint_overlap_cOnlyExcluded.RData')
-load('../data/200904_swapExp_marginalDE_varyingPint_overlap_cOnlyExcluded_permBody.RData')
-load('../data/200904_swapExp_marginalDE_varyingPint_overlap_cOnlyExcluded_permHead.RData')
+load('archive/R/210209_swapExp_marginalDEfdr01_varyingPint_overlap_cOnlyExcluded_w5000.RData')
+load('archive/R/210209_swapExp_marginalDEfdr01_varyingPint_overlap_cOnlyExcluded_permBody_w5000.RData')
+load('archive/R/210209_swapExp_marginalDEfdr01_varyingPint_overlap_cOnlyExcluded_permHead_w5000.RData')
 library(ggplot2)
 library(ggpubr)
+library(cowplot)
+library(patchwork)
+library(dplyr)
 #Fraction overlapping SNPs
-ylim <- range(c(overlap_swapExpSelection_DEfdr05_varyingSelP_body.perm$nOverlapSNPs_geno.perm/overlap_swapExpSelection_DEfdr05_varyingSelP_body.perm$nSNPs,
-                overlap_swapExpSelection_DEfdr05_varyingSelP_body.perm$nOverlapSNPs_geno/overlap_swapExpSelection_DEfdr05_varyingSelP_body.perm$nSNPs,
-                overlap_swapExpSelection_DEfdr05_varyingSelP_body.perm$nOverlapSNPs_diet.perm/overlap_swapExpSelection_DEfdr05_varyingSelP_body.perm$nSNPs,
-                overlap_swapExpSelection_DEfdr05_varyingSelP_body.perm$nOverlapSNPs_diet/overlap_swapExpSelection_DEfdr05_varyingSelP_body.perm$nSNPs))
+ylim_body <- range(c(overlap_swapExpSelection_DEfdr01_varyingSelP_body.perm$nOverlapSNPs_geno.perm/      
+                overlap_swapExpSelection_DEfdr01_varyingSelP_body.perm$nSNPs,
+
+                overlap_swapExpSelection_DEfdr01_varyingSelP_body.perm$nOverlapSNPs_geno/overlap_swapExpSelection_DEfdr01_varyingSelP_body.perm$nSNPs,
+
+                overlap_swapExpSelection_DEfdr01_varyingSelP_body.perm$nOverlapSNPs_diet.perm/overlap_swapExpSelection_DEfdr01_varyingSelP_body.perm$nSNPs,
+
+                overlap_swapExpSelection_DEfdr01_varyingSelP_body.perm$nOverlapSNPs_diet/overlap_swapExpSelection_DEfdr01_varyingSelP_body.perm$nSNPs))
 #Plot body
-g.geno <- ggplot(data = overlap_swapExpSelection_DEfdr05_varyingSelP_body.perm, aes(y = nOverlapSNPs_geno.perm/nSNPs, x = factor(-log10(p)))) +
+g.geno_b <- ggplot(data = overlap_swapExpSelection_DEfdr01_varyingSelP_body.perm, 
+                 aes(y = nOverlapSNPs_geno.perm/nSNPs, 
+                     x = factor(-log10(p)))) +
   geom_boxplot(key_glyph = 'rect') +
-  geom_line(data = overlap_swapExpSelection_DEfdr05_varyingSelP_body, mapping = aes(y = nOverlapSNPs_geno/nSNPs, x = factor(-log10(p)), group = 1, size = 1)) + #x = p_int, y = overlap_HS,
+  geom_line(data = overlap_swapExpSelection_DEfdr01_varyingSelP_body, 
+            mapping = aes(y = nOverlapSNPs_geno/nSNPs, 
+                          x = factor(-log10(p))), 
+            group = 1, linewidth = 1) +
+  theme_classic() +
   theme(legend.position = 'none',
-        axis.text=element_text(size=15),
-        axis.title=element_text(size=20),
-        plot.title = element_text(hjust = 0.5, size = 25),
-        plot.subtitle = element_text(hjust = 0.5, size = 15)) + ylim(ylim) +
-  ylab('Fraction of selected SNPs in DE genes') + xlab(expression('-log10(' ~ p[int] ~ ')')) +
+        axis.text=element_text(size=8),
+        axis.title=element_text(size=8),
+        plot.title = element_text(hjust = 0.5, size = 10),
+        plot.subtitle = element_text(hjust = 0.5, size = 8)) + ylim(ylim_body) +
+  ylab('Fraction of selected SNPs in DE genes') + 
+  xlab(expression('-log10(' ~ p[int] ~ ')')) +
   ggtitle('Selection History Effect')
-
-g.diet <- ggplot(data = overlap_swapExpSelection_DEfdr05_varyingSelP_body.perm, aes(y = nOverlapSNPs_diet.perm/nSNPs, x = factor(-log10(p)))) +
-  geom_boxplot(key_glyph = 'rect') +
-  geom_line(data = overlap_swapExpSelection_DEfdr05_varyingSelP_body, mapping = aes(y = nOverlapSNPs_diet/nSNPs, x = factor(-log10(p)), group = 1, size = 1)) + #x = p_int, y = overlap_HS,
-  theme(legend.position = 'none',
-        axis.text=element_text(size=15),
-        axis.title=element_text(size=20),
-        plot.title = element_text(hjust = 0.5, size = 25),
-        plot.subtitle = element_text(hjust = 0.5, size = 15)) + ylim(ylim) +
-  ylab('Fraction of selected SNPs in DE genes') + xlab(expression('-log10(' ~ p[int] ~ ')')) +
-  ggtitle('Plastic Effect')
-
-png('../figures/fig_4_swapMarginalDE_pInt_overlap_body.png', width = 1200, height = 800)
-ggarrange(g.geno, g.diet)
-dev.off()
 
 #Plot head
-ylim <- range(c(overlap_swapExpSelection_DEfdr05_varyingSelP_head.perm$nOverlapSNPs_geno.perm/overlap_swapExpSelection_DEfdr05_varyingSelP_head.perm$nSNPs,
-                overlap_swapExpSelection_DEfdr05_varyingSelP_head.perm$nOverlapSNPs_geno/overlap_swapExpSelection_DEfdr05_varyingSelP_head.perm$nSNPs,
-                overlap_swapExpSelection_DEfdr05_varyingSelP_head.perm$nOverlapSNPs_diet.perm/overlap_swapExpSelection_DEfdr05_varyingSelP_head.perm$nSNPs,
-                overlap_swapExpSelection_DEfdr05_varyingSelP_head.perm$nOverlapSNPs_diet/overlap_swapExpSelection_DEfdr05_varyingSelP_head.perm$nSNPs))
+ylim_head <- range(c(overlap_swapExpSelection_DEfdr01_varyingSelP_head.perm$nOverlapSNPs_geno.perm/overlap_swapExpSelection_DEfdr01_varyingSelP_head.perm$nSNPs,
+                overlap_swapExpSelection_DEfdr01_varyingSelP_head.perm$nOverlapSNPs_geno/overlap_swapExpSelection_DEfdr01_varyingSelP_head.perm$nSNPs,
+                overlap_swapExpSelection_DEfdr01_varyingSelP_head.perm$nOverlapSNPs_diet.perm/overlap_swapExpSelection_DEfdr01_varyingSelP_head.perm$nSNPs,
+                overlap_swapExpSelection_DEfdr01_varyingSelP_head.perm$nOverlapSNPs_diet/overlap_swapExpSelection_DEfdr01_varyingSelP_head.perm$nSNPs))
 
-g.geno <- ggplot(data = overlap_swapExpSelection_DEfdr05_varyingSelP_head.perm, aes(y = nOverlapSNPs_geno.perm/nSNPs, x = factor(-log10(p)))) +
+g.geno_h <- ggplot(data = overlap_swapExpSelection_DEfdr01_varyingSelP_head.perm, 
+                 aes(y = nOverlapSNPs_geno.perm/nSNPs, x = factor(-log10(p)))) +
   geom_boxplot(key_glyph = 'rect') +
-  geom_line(data = overlap_swapExpSelection_DEfdr05_varyingSelP_head, mapping = aes(y = nOverlapSNPs_geno/nSNPs, x = factor(-log10(p)), group = 1, size = 1)) + #x = p_int, y = overlap_HS,
+  geom_line(data = overlap_swapExpSelection_DEfdr01_varyingSelP_head,
+            mapping = aes(y = nOverlapSNPs_geno/nSNPs, x = factor(-log10(p))), 
+            group = 1, size = 1) + 
+  theme_classic() +
   theme(legend.position = 'none',
-        axis.text=element_text(size=15),
-        axis.title=element_text(size=20),
-        plot.title = element_text(hjust = 0.5, size = 25),
-        plot.subtitle = element_text(hjust = 0.5, size = 15)) + ylim(ylim) +
+        axis.text=element_text(size=8),
+        axis.title=element_text(size=8),
+        plot.title = element_text(hjust = 0.5, size = 10),
+        plot.subtitle = element_text(hjust = 0.5, size = 8)) + ylim(ylim_head) +
   ylab('Fraction of selected SNPs in DE genes') + xlab(expression('-log10(' ~ p[int] ~ ')')) +
-  ggtitle('Selection History Effect')
+  ggtitle('Selection History Effect - HEAD')
 
-g.diet <- ggplot(data = overlap_swapExpSelection_DEfdr05_varyingSelP_head.perm, aes(y = nOverlapSNPs_diet.perm/nSNPs, x = factor(-log10(p)))) +
+save_plot("test.png", g.geno_h + g.geno_b)
+
+ylim = c(0.06, 0.22)
+
+dat.perm = bind_rows(Head = overlap_swapExpSelection_DEfdr01_varyingSelP_head.perm, 
+                     Body = overlap_swapExpSelection_DEfdr01_varyingSelP_body.perm, 
+                     .id = "tissue")
+dat.obs = bind_rows(Head = overlap_swapExpSelection_DEfdr01_varyingSelP_head, 
+                     Body = overlap_swapExpSelection_DEfdr01_varyingSelP_body, 
+                     .id = "tissue")
+g.geno_facet <- ggplot(data = dat.perm, 
+                       aes(y = nOverlapSNPs_geno.perm/nSNPs,
+                           x = factor(-log10(p)))) +
   geom_boxplot(key_glyph = 'rect') +
-  geom_line(data = overlap_swapExpSelection_DEfdr05_varyingSelP_head, mapping = aes(y = nOverlapSNPs_diet/nSNPs, x = factor(-log10(p)), group = 1, size = 1)) + #x = p_int, y = overlap_HS,
+  geom_line(data = dat.obs,
+            mapping = aes(y = nOverlapSNPs_geno/nSNPs, x = factor(-log10(p))), 
+            group = 1, size = 1) + 
+  facet_grid(cols = vars(tissue)) +
+  theme_classic() +
   theme(legend.position = 'none',
-        axis.text=element_text(size=15),
-        axis.title=element_text(size=20),
-        plot.title = element_text(hjust = 0.5, size = 25),
-        plot.subtitle = element_text(hjust = 0.5, size = 15)) + ylim(ylim) +
-  ylab('Fraction of selected SNPs in DE genes') + xlab(expression('-log10(' ~ p[int] ~ ')')) +
-  ggtitle('Plastic Effect')
+        axis.text=element_text(size=8),
+        axis.title=element_text(size=8),
+        plot.title = element_text(hjust = 0.5, size = 10),
+        plot.subtitle = element_text(hjust = 0.5, size = 8)) + ylim(ylim) +
+  ylab('Fraction of selected SNPs in DE genes') + xlab(expression('-log10(' ~ p[int] ~ ')')) 
+  
+save_plot("test.png", g.geno_facet )
 
-png('../figures/fig_4_swapMarginalDE_pInt_overlap_head.png', width = 1200, height = 800)
-ggarrange(g.geno, g.diet)
-dev.off()
 
-#Plastic vs Evolved DE effects. Like fig2 here https://journals.plos.org/plosgenetics/article?id=10.1371/journal.pgen.1006336
-load('../data/200608_DEres_GRanges.RData')
-
-keep <- dmel.gff_swapGenes_body$padj_diet < .05 & dmel.gff_swapGenes_body$padj_geno < .05
-png('../figures/fig_4_plastic_vs_evol_DE_body.png', 1200, 1200)
-par(mar = c(5,6,4,2) + .1)
-plot(dmel.gff_swapGenes_body$lfc_diet[keep], dmel.gff_swapGenes_body$lfc_geno[keep],
-     xlab = 'Plastic Effect', ylab = 'Selection History Effect', cex.lab = 3, cex.axis = 2, main = '', cex.main = 3)
-abline(h = 0, lty = 2)
-abline(v = 0, lty = 2)
-grid()
-dev.off()
-
-keep <- dmel.gff_swapGenes_head$padj_diet < .05 & dmel.gff_swapGenes_head$padj_geno < .05
-png('../figures/fig_4_plastic_vs_evol_DE_head.png', 1200, 1200)
-par(mar = c(5,6,4,2) + .1)
-plot(dmel.gff_swapGenes_head$lfc_diet[keep], dmel.gff_swapGenes_head$lfc_geno[keep],
-     xlab = 'Plastic Effect', ylab = 'Selection History Effect', cex.lab = 3, cex.axis = 2, main = '', cex.main = 3)
-abline(h = 0, lty = 2)
-abline(v = 0, lty = 2)
-grid()
-dev.off()
--- code/20221101_differential_expression_volcanos.R
+++ code/20221101_differential_expression_volcanos.R
@@ -1,72 +1,72 @@
 #Volcano plots
 options(stringsAsFactors = F)
-swapExp_DESeqRes_body <- read.table('../data/Results_DESeq_SWAPexperiment_body_May12.txt', header = T)
-swapExp_DESeqRes_head <- read.table('../data/Results_DESeq_SWAPexperiment_head_May12.txt', header = T)
+swapExp_DESeqRes_body <- read.table('data/Results_DESeq_SWAPexperiment_body_May12.txt', header = T)
+swapExp_DESeqRes_head <- read.table('data/Results_DESeq_SWAPexperiment_head_May12.txt', header = T)
 
 #body
 DE.fdr <- .01
-col <- rep('gray', nrow(swapExp_DESeqRes_body))
-col[swapExp_DESeqRes_body$padj_geno < DE.fdr] <- 'red'
-png('../figures/DEgeno_body_volcano.png', 1200, 1200)
-par(mar = c(5,5,4,2))
-plot(swapExp_DESeqRes_body$lfc_geno, -log10(swapExp_DESeqRes_body$padj_geno), col = col, pch = 19, cex = 1,
-     xlab = 'Log2 fold change', ylab = '-log10(p)', cex.lab = 2.5, cex.axis = 2)
-abline(h = -log10(DE.fdr), lty = 2, lwd = 2)
-dev.off()
-
-#head
-DE.fdr <- .01
-col <- rep('gray', nrow(swapExp_DESeqRes_head))
-col[swapExp_DESeqRes_head$padj_geno < DE.fdr] <- 'red'
-png('../figures/DEgeno_head_volcano.png', 1200, 1200)
-par(mar = c(5,5,4,2))
-plot(swapExp_DESeqRes_head$lfc_geno, -log10(swapExp_DESeqRes_head$padj_geno), col = col, pch = 19, cex = 1,
-     xlab = 'Log2 fold change', ylab = '-log10(p)', cex.lab = 2.5, cex.axis = 2)
-abline(h = -log10(DE.fdr), lty = 2, lwd = 2)
-dev.off()
-
-#Combined
-xlim <- range(c(swapExp_DESeqRes_body$lfc_geno, swapExp_DESeqRes_head$lfc_geno))
-ylim <- range(-log10(c(swapExp_DESeqRes_body$padj_geno, swapExp_DESeqRes_head$padj_geno)))
-
-png('../figures/DEgeno_bodyANDhead_volcano.png', width = 2*1200, height = 1200)
-par(mar = c(5,5,4,2), mfrow = 1:2)
-
-col <- rep('gray', nrow(swapExp_DESeqRes_body))
-col[swapExp_DESeqRes_body$padj_geno < DE.fdr] <- 'red'
-plot(swapExp_DESeqRes_body$lfc_geno, -log10(swapExp_DESeqRes_body$padj_geno), col = col, pch = 19, cex = 1,
-     xlab = 'Log2 fold change', ylab = '-log10(p)', cex.lab = 2.5, cex.axis = 2, main = 'Body', cex.main = 3,
-     xlim = xlim, ylim = ylim)
-abline(h = -log10(DE.fdr), lty = 2, lwd = 2)
-
-col <- rep('gray', nrow(swapExp_DESeqRes_head))
-col[swapExp_DESeqRes_head$padj_geno < DE.fdr] <- 'red'
-plot(swapExp_DESeqRes_head$lfc_geno, -log10(swapExp_DESeqRes_head$padj_geno), col = col, pch = 19, cex = 1,
-     xlab = 'Log2 fold change', ylab = '-log10(p)', cex.lab = 2.5, cex.axis = 2, main = 'Head', cex.main = 3,
-     xlim = xlim, ylim = ylim)
-abline(h = -log10(DE.fdr), lty = 2, lwd = 2)
-dev.off()
 
 #ggplot facet version
-dat <- data.frame(tissue = rep(c('Body', "Head"), c(nrow(swapExp_DESeqRes_body), nrow(swapExp_DESeqRes_head))),
-                  lfc_geno = c(swapExp_DESeqRes_body$lfc_geno, swapExp_DESeqRes_head$lfc_geno),
-                  p_geno = c(swapExp_DESeqRes_body$padj_geno, swapExp_DESeqRes_head$padj_geno))
+dat <- data.frame(tissue = rep(c('Body', "Head"), 
+                               c(nrow(swapExp_DESeqRes_body), 
+                                 nrow(swapExp_DESeqRes_head))),
+                  lfc_geno = c(swapExp_DESeqRes_body$lfc_geno, 
+                               swapExp_DESeqRes_head$lfc_geno),
+                  p_geno = c(swapExp_DESeqRes_body$padj_geno,                
+                             swapExp_DESeqRes_head$padj_geno))
 dat$sign <- 0
 dat$sign[dat$p_geno < DE.fdr] <- 1
 
+table(dat$sign, dat$tissue)
+
 g <- ggplot(data = dat, aes(x = lfc_geno, y = -log10(p_geno), color = sign)) +
-  geom_point(size = 4) + theme_classic() +
+  geom_point(size = 0.8) + theme_classic() +
   facet_grid(cols = vars(tissue)) +
   # scale_color_manual(values = c('0' = 'black', '1' = 'red')) +
   theme(legend.position = 'none',
-        axis.text=element_text(size=35),
-        axis.title=element_text(size=40),
-        plot.title = element_text(hjust = 0.5, size = 25),
-        plot.subtitle = element_text(hjust = 0.5, size = 15),
-        strip.text.x = element_text(size = 40, face = 'bold')) +
+        axis.text=element_text(size=8),
+        axis.title=element_text(size=8),
+        plot.title = element_text(size = 10),
+        plot.subtitle = element_text(size = 8),
+        strip.text.x = element_text(size = 8)) +
   xlab('Log2 fold change') + ylab(expression('-log10(' ~ p[DE] ~ ')')) +
   geom_hline(yintercept = -log10(DE.fdr), linetype = 'dashed')
 
-png('../figures/DEgeno_bodyANDhead_volcano_ggplot_v2.png', width = 1200, height = 1200)
+png('figures/DEgeno_bodyANDhead_volcano_ggplot_v2.png', 
+    width = 3, height = 3, unit = "in", res = 300)
 print(g)
 dev.off()
+
+load('archive/R/210209_swapExp_marginalDEfdr01_varyingPint_overlap_cOnlyExcluded_w5000.RData')
+load('archive/R/210209_swapExp_marginalDEfdr01_varyingPint_overlap_cOnlyExcluded_permBody_w5000.RData')
+load('archive/R/210209_swapExp_marginalDEfdr01_varyingPint_overlap_cOnlyExcluded_permHead_w5000.RData')
+library(ggplot2)
+library(ggpubr)
+library(cowplot)
+library(patchwork)
+library(dplyr)
+
+dat.perm = bind_rows(Head = overlap_swapExpSelection_DEfdr01_varyingSelP_head.perm, 
+                     Body = overlap_swapExpSelection_DEfdr01_varyingSelP_body.perm, 
+                     .id = "tissue")
+dat.obs = bind_rows(Head = overlap_swapExpSelection_DEfdr01_varyingSelP_head, 
+                    Body = overlap_swapExpSelection_DEfdr01_varyingSelP_body, 
+                    .id = "tissue")
+g.geno_facet <- ggplot(data = dat.perm, 
+                       aes(y = nOverlapSNPs_geno.perm/nSNPs,
+                           x = factor(-log10(p)))) +
+  geom_boxplot(key_glyph = 'rect') +
+  geom_line(data = dat.obs,
+            mapping = aes(y = nOverlapSNPs_geno/nSNPs, x = factor(-log10(p))), 
+            group = 1, size = 1, color = 2) + 
+  facet_grid(cols = vars(tissue)) +
+  theme_classic() +
+  theme(legend.position = 'none',
+        axis.text=element_text(size=8),
+        axis.title=element_text(size=8),
+        plot.title = element_text(size = 10),
+        plot.subtitle = element_text(size = 8)) + ylim(ylim) +
+  ylab('Fraction of selected SNPs in DE genes') + xlab(expression('-log10(' ~ p[int] ~ ')')) 
+  
+save_plot("figures/DE.png", g + ggtitle("A.") + g.geno_facet + ggtitle("B.") ,
+          base_width = 5.5, base_height = 3 )
